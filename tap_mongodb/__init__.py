371ab68a3d5f7ccba8eae6bd11c56d2df9a50002
assketick <tuhlin.nekit@yandex.ru>
2024-01-29 19:27:56 +0300

    *

--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -262,12 +262,9 @@ def get_connection_string(config: Dict):
     connection_query = {
         'readPreference': 'secondaryPreferred',
         'authSource': config['auth_database'],
-        'timeoutMS': 3600000,
-        'keepAlive': 'true',
-        'poolSize': 30,
-        'autoReconnect': 'true',
-        'socketTimeoutMS': 360000,
-        'connectTimeoutMS': 360000
+        'socketTimeoutMS': 3600000,
+        'connectTimeoutMS': 3600000,
+        'serverSelectionTimeoutMS': 3600000,
     }
 
     if config.get('replica_set'):
